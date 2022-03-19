53461217f85e4bedfe9b98027791bb8fd1b4a2aa
Tavish-153 <83607020+Tavish-153@users.noreply.github.com>
2022-03-19 14:11:37 +0530

    Uhh it's brok
    NAN%

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,11 +81,12 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-                ['Twish engine'],
-                ['Twish','Twish',Porter for andriod and maker of Twish engine','https://youtube.com/channel/UCSCEwuAExtLzGTQUFfUefmg','FFFFFF'],
-		        ['Android Port'],
-		        ['Saw (M.A. Jigsaw)','saw','Main Coder','https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 'F73838'],
-		        [''],
+            ['Twish engine'],
+            ['Twish','Twish',Porter for andriod and maker of Twish engine','https://youtube.com/channel/UCSCEwuAExtLzGTQUFfUefmg','FFFFFF'],
+            [''],
+		    ['Android Port'],
+		    ['Saw (M.A. Jigsaw)','saw','Main Coder','https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 'F73838'],
+		    [''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',						'https://twitter.com/RiverOaken',		'C30085'],
