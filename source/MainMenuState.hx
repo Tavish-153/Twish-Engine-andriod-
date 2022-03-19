568f2770176f2ae7a7a6d3eb38644d0538c8c72e
Tavish-153 <83607020+Tavish-153@users.noreply.github.com>
2022-03-19 14:03:57 +0530

    Uhh
    NaN

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -117,7 +117,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
 			//menuItem.screenCenter(X);
-			menuItem.X(-100)
+			menuItem.X(-100);
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
